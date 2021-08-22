51395459a0c60a93c084b7c5a4ed4c9fe90dfb45
Krishan Shamod <59104070+krishanshamod@users.noreply.github.com>
2021-08-22 21:16:32 +0530

    v2 - final code

--- src/java/com/company/v2/Main.java
+++ src/java/com/company/v2/Main.java
@@ -1,20 +1,47 @@
 package com.company.v2;
 
-public class Main {
-    public static void main(String[] args){
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
-        if(args.length == 0){
+public class Main {
+
+    //SOLID PRINCIPLES --> ALL THE SOLID PRINCIPLES ARE VIOLATED HERE!!!
+    public static void main(String[] args) throws IOException {
+
+        //Make sure the argument is correct
+        if (args.length == 0) {
             System.out.println("Please provide the operation as an argument");
             return;
         }
 
         String operator = args[0];
 
-        if(!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))){
+        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))) {
             System.out.println("Please provide add,sub or mul as the argument");
             return;
         }
 
-        System.out.println(args[0]);
+        //read the numbers from txt file
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("C:\\Users\\Krishan Shamod\\Documents\\Coding\\GitHub\\calculator\\numbers.txt")
+        );
+
+        double number1 = Double.parseDouble(numbersStrs.get(0));
+        double number2 = Double.parseDouble(numbersStrs.get(1));
+
+        double result = 0;
+
+        if (operator.equals("add")) {
+            result = number1 + number2;
+        } else if (operator.equals("sub")) {
+            result = number1 - number2;
+        } else if (operator.equals("mul")) {
+            result = number1 * number2;
+        }
+
+        System.out.println("The result is " + result);
+
     }
 }
